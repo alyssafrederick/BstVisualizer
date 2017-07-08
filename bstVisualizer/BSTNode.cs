ec1edad4af62035018856f088ea6120c89bee896
alyssafrederick <peaceloveyummydance@gmail.com>
2017-07-07 17:50:08 -0700

    Initial commit

---
+++ bstVisualizer/BSTNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bstVisualizer
+{
+    class BSTNode<T> where T : IComparable
+    {
+        public T Value;
+        public BSTNode<T> parent;
+        public BSTNode<T> leftChild;
+        public BSTNode<T> rightChild;
+
+        public BSTNode(T value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ bstVisualizer/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bstVisualizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            Label number = new Label();           
+            Ellipse ell = new Ellipse();
+
+            ell.Height = ell.Width = 40;
+            ell.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 255));
+            number.Content = "hi";
+            number.Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+
+
+            ell.Margin = number.Margin = new Thickness(200, 100, 20, 100);
+            ell.HorizontalAlignment = number.HorizontalAlignment = HorizontalAlignment.Center;
+            ell.VerticalAlignment = number.VerticalAlignment = VerticalAlignment.Center;
+
+            grid.Children.Add(ell);
+            grid.Children.Add(number);
+        }
+    }
+}
